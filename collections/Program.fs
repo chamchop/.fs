834cb2567dff2ec9917f396b5a8f180704cf430a
chamchop <ocfmiller@protonmail.com>
2024-02-02 15:47:56 +0000

    conditionals, loops, collections

---
+++ collections/Program.fs
@@ -0,0 +1,29 @@
+﻿open System
+open System.IO
+
+let printMeanScore (row : string) =
+    let elements = row.Split('\t')
+    printfn "%s" row
+
+let summarise filePath =
+    let rows = File.ReadAllLines filePath
+    let studentCount = (rows |> Array.length) - 1
+    printfn "student count %i" studentCount
+    rows
+    |> Array.skip 1
+    |> Array.iter printMeanScore    
+
+[<EntryPoint>]
+let main argv = 
+    if argv.Length = 1 then
+        let filePath = argv.[0]
+        if File.Exists filePath then        
+            printfn "processing %s" filePath
+            summarise filePath
+            0
+        else
+            printfn "File not found: %s" filePath
+            2
+    else
+        printfn "Please specify a file"
+        1
---
+++ intro/Program.fs
@@ -0,0 +1,43 @@
+﻿open System
+
+let sayHello person = 
+    printfn "Hello %s from F#!" person    
+
+let isValid person = 
+    String.IsNullOrWhiteSpace person |> not
+
+let isAllowed person =
+    person <> "Eve"
+
+[<EntryPoint>]
+let main argv = 
+    argv 
+    |> Array.filter isValid 
+    |> Array.filter isAllowed
+    |> Array.iter sayHello 
+    printfn "Nice to meet you"
+    0  
+
+// [<EntryPoint>]
+// let main argv = 
+//     for person in argv do
+//         printfn "Hello %s from F#!" person
+//     printfn "Nice to meet you."
+//     0
+
+// [<EntryPoint>]
+// let main argv = 
+//     for i in 0..argv.Length - 1 do
+//         let person = argv.[i]
+//         printfn "Hello %s from F#!" person
+//     printfn "Nice to meet you."
+//     0
+
+// [<EntryPoint>]
+// let main argv = 
+//     let person = 
+//         if argv.Length > 0 then
+//             argv.[0]
+//         else "name"
+//     printfn "Hello %s from F#!" person
+//     0
